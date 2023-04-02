eb65015b23abe0eeeb3a1c3f294ebc731bf13fb5
evelinaNor <eva19943@gmail.com>
2023-04-02 14:42:11 +0300

    multiplication table with 5

---
+++ Part6Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part6Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                int sum = 5 * i;
+                Console.WriteLine($"5 x {i} = {sum}");
+            }
+
+        }
+    }
+}
