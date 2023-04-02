9e37b58b907696bf852c809aec8bc79674a4d49d
evelinaNor <eva19943@gmail.com>
2023-04-02 13:58:08 +0300

    output of addition, subtraction, multiplication, division operators

---
+++ Part5Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part5Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 5;
+            int sum = a + b;
+            int sum2 = a - b;
+            int sum3 = a * b;
+            float sum4 =(float) a / b;
+
+            Console.WriteLine($"{a} / {b} = {sum}");
+            Console.WriteLine($"{a} / {b} = {sum2}");
+            Console.WriteLine($"{a} / {b} = {sum3}");
+            Console.WriteLine($"{a} / {b} = {sum4}");
+        }
+    }
+}
