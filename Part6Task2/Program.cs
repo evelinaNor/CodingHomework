bcc20be5aef433e4febb73f28c2b7c11052d97bb
evelinaNor <eva19943@gmail.com>
2023-04-02 14:59:41 +0300

    Multiplication by seperate digit of integer variable that contains two-digits

---
+++ Part6Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part6Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 32;
+            int digit1 = number / 10;
+            int digit2 = number % 10;
+            int result = digit1 * digit2;
+
+            Console.WriteLine(number);
+            Console.WriteLine($"{digit1} * {digit2} = {result}");
+        }
+    }
+}
