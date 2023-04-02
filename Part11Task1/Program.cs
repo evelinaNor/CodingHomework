8b9692ad7434e6b455b84186d596025cf40bbfcb
evelinaNor <eva19943@gmail.com>
2023-04-02 15:58:37 +0300

    Comparison between numbers with if and else if statement

---
+++ Part11Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part11Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter three numbers");
+            Console.WriteLine("Enter first number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 > number2)
+               Console.WriteLine($"The first number {number1} is bigger than the second number {number2}");
+            else if (number1 == number2)
+                Console.WriteLine($"The first number {number1} and the second number {number1} are equal");
+            else if (number2 > number3)
+                Console.WriteLine($"The second number {number2} is bigger than the third number {number3}");
+            else if (number3 > number1)
+                Console.WriteLine($"The third number {number3} is bigger than the first number {number1}");
+        }
+    }
+}
