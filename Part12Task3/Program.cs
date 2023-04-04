c78f522760614c3118d8acb7d1d3d9b7ebaf4b0a
evelinaNor <eva19943@gmail.com>
2023-04-04 18:55:58 +0300

    Operators && and ||

---
+++ Part12Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part12Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter two numbers:");
+       
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 > number2 || number1 == 0) 
+            {
+                Console.WriteLine($"{number1} is bigger than {number2} or is equal to 0");
+            }
+
+            if (number2 > number1 || number2 == 5)
+            {
+                Console.WriteLine($"{number2} is bigger than {number1} or is equal to 5");
+            }
+
+            if (number1 > number2 && number1 == 20)
+            {
+                Console.WriteLine($"{number1} is bigger than {number2} and is equal to 20");
+            }
+
+            if (number2 > number1 && number2 < 100)
+            {
+                Console.WriteLine($"{number2} is bigger than {number1} and is less than 100");
+            }
+        }
+    }
+}
