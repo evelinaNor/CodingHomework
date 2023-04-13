bec2d620d9a48c2450b062152b3532d24a53dc13
evelinaNor <eva19943@gmail.com>
2023-04-13 19:37:00 +0300

    entered number increasing each time by higher powers

---
+++ Part19Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part19Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Please enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int pow = 1;
+            int result = num;
+
+            while (result < 1500)
+            {
+                Console.WriteLine(result);
+                pow++;
+                result = (int)Math.Pow(num, pow);
+            }
+        }
+    }
+}
